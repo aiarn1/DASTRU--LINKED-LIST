9fee010fbff7323fd0db75e4a4044334bf0a6603
Aian Cuyos <aiarn1006@gmail.com>
2026-01-22 17:06:02 +0800

    Add files via upload

---
+++ Cuyos_act6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Cuyos_act6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Louis Aian Cuyos
+            // ACTIVITY 6: Inserting Nodes to the Linked List
+
+            // Create Linked List
+            LinkedList<string> list = new LinkedList<string>();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            // Title
+            Console.WriteLine("--- Enter 5 Favorite Teacher Last Names ---\n");
+
+            // Loop to Input 5 Names
+            for (int i = 0; i < 5; i++)
+            {
+                // Input Favorite Teacher Last Name
+                Console.Write("Favorite Teacher's "+ (i + 1) +  ": ");
+                string name = Console.ReadLine();
+                list.AddLast(name);
+            }
+            
+            // Display Output
+            Console.WriteLine("\n----- List of Favorite Teachers ----- ");
+            foreach (string name in list)
+            {         
+                Console.Write(name + " -> ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Cuyos_act6/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Cuyos_act6")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cuyos_act6")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3729bf1f-f06b-48bd-9d23-df4a1eabbe9e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
