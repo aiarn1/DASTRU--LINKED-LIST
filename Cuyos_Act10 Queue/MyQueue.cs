3a55dd78f598e4ed50326c047c6d1ab66b17acd7
Aian Cuyos <aiarn1006@gmail.com>
2026-03-05 13:42:44 +0800

    Add files via upload

---
+++ Cuyos_Act10 Queue/MyQueue.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_Act10_Queue
+{
+     class MyQueue
+    {
+        // LinkList implementation of Queue
+        private Queue<int> queue = new Queue<int>();
+
+        // Enqueue method to add an element 
+        public void Enqueue(int value)
+        {
+            queue.Enqueue(value);
+            Console.WriteLine("Added: " + value);
+        }
+        // Dequeue method to remove an element
+        public void Dequeue()
+        {
+            if (queue.Count == 0)
+                throw new InvalidOperationException("Queue is empty.");
+
+            Console.WriteLine("Removed: " + queue.Dequeue());
+        }
+        // Display method to show all elements in the queue
+        public void Display()
+        {
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Queue is empty.");
+                return;
+            }
+            foreach (var item in queue)
+                Console.Write("Updated Queue: " + item + " ");
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ Cuyos_Act10 Queue/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuyos_Act10_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a queue and display menu
+            MyQueue q = new MyQueue(); 
+            int choice;
+            do
+            { 
+                // Selection menu
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\n--- Queue Menu ---");
+                Console.WriteLine("1. Enqueue");
+                Console.WriteLine("2. Dequeue");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("0. Exit");
+                Console.Write("Choose a number: ");
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.Write("\nEnter number: ");
+                            int num = int.Parse(Console.ReadLine());
+                            q.Enqueue(num);
+                            break;
+                        case 2:
+                            q.Dequeue();
+                            break;
+                        case 3:
+                            q.Display();
+                            break;
+                        case 0:
+                            Console.WriteLine("Program ended.");
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("\nInvalid choice Please select 1-3 only.");
+                            break;
+                    }
+                }
+                // Handle any exceptions 
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message); choice = -1;
+                }
+            } while (choice != 0);
+        }
+    }
+}
+
